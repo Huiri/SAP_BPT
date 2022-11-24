e50a9019a2238163e0016462aa67c233a65c9dd4
희리 문 <gmlfltj@naver.com>
2022-11-24 11:20:40 +0000

    ADD : 주석 추가

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,6 +1,7 @@
 /**
  * eslint-disable @sap/ui5-jsdocs/no-jsdoc
  */
+// 앱 설정 유지 기능
 
 sap.ui.define([
         "sap/ui/core/UIComponent",
@@ -21,6 +22,10 @@ sap.ui.define([
              * @override
              */
             init: function () {
+                //구성요소가 인스턴스화 될 때 SAPUI5에 의해 자동으로 시작
+                // 구성요소의 메타데이터 섹션에서 설명자 파일에 대한 참조 정의
+                // !init 함수 재정의 시 구성요소의 init 함수 호출 -> 나중에 라우터 초기화
+
                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
 
