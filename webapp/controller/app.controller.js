e6ed89ea236426c9c8527b82743780acadaca5aa
희리 문 <gmlfltj@naver.com>
2022-11-24 11:22:54 +0000

    REMOVE : 불필요한 주석 삭제

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,21 +1,9 @@
 sap.ui.define([
-    // "sap/ui/core/mvc/Controller"
     'sap/ui/core/mvc/Controller'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    //  function(MessageToast, Controller) {
-    //     "use strict";
-    
-    //     return Controller.extend("sap.m.sample.MessageToast.controller.MessageToast", {
-    
-    //         handleMessageToastPress: function(oEvent) {
-    //             var msg = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy\r\n eirmod.';
-    //             MessageToast.show(msg);
-    //         }
-    //     });
-    // },
     function (Controller) {
         "use strict";
 
