32a340801a916b0165d7593362bca3e119c1d60b
희리 문 <gmlfltj@naver.com>
2022-11-28 08:42:07 +0000

    FEAT : sort 기능 추가

--- webapp/controller/Request.controller.js
+++ webapp/controller/Request.controller.js
@@ -1,66 +1,117 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller', "sap/m/MessageToast",
     "../model/formatter", "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator", 
-],
+    "sap/ui/model/FilterOperator", "sap/ui/model/Sorter", "sap/ui/core/Fragment"
+],//사용할 모듈 선언
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, formatter, Filter, FilterOperator) {
+    function (Controller, MessageToast, formatter, Filter, FilterOperator, Sorter, Fragment) {//사용할 모듈의 별칭 선언(순서 맞춰 주어야 오류 발생 X)
         "use strict";
 
-        return Controller.extend("project1.controller.Request", {
+        return Controller.extend("project1.controller.Request", { // 소괄호 안의 경로 파일을 컨트롤러로 사용하겠다는 선언
             formatter:formatter, //해당 이름의 메소드로 사용하겠다고 선언
             
             onInit: function () {
-        
             },
+            
+            //검색을 위한 함수
             onSearch : function () {
-                let ReqNum = this.byId("ReqNum").getValue();
-                // this.byId("ReqNum").getText();
-                let ReqGood = this.byId("ReqGood").getValue();
+                let ReqNum = this.byId("ReqNum").getValue(); 
+                let ReqGood = this.byId("ReqGood").getValue(); 
                 let ReqDate = this.byId("ReqDate").getValue();
                 let Requester = this.byId("Requester").getValue();
                 let ReqStatus = this.byId("ReqStatus").getSelectedKey();
 
                 if(ReqDate){
-                    let ReqYear = ReqDate.split(". ")[0];
-                    let ReqMonth = ReqDate.split(". ")[1].padStart(2,'0');
-                    ReqDate = ReqYear + "-" + ReqMonth;
+                    let ReqYear = ReqDate.split(". ")[0]; //". " 기준으로 잘라서 앞의 연도 값을 변수에 저장
+                    let ReqMonth = ReqDate.split(". ")[1].padStart(2,'0'); //". " 기준으로 잘라서 뒤의 일이 2자리가 아닐 경우 0을 붙여서 변수에 저장
+                    ReqDate = ReqYear + "-" + ReqMonth; //형식을 맞추어주기 위해서 연도-일자 형식으로 ReqDate 값에 저장
                 }
 
-                var aFilter = [];
+                var aFilter = []; // 여러 필터 값을 지정할 수 있도록 배열로 받기
 
-                if(ReqNum) {aFilter.push(new Filter("ReqNum", FilterOperator.Contains, ReqNum))}
-                if(ReqGood) {aFilter.push(new Filter("ReqGood", FilterOperator.Contains, ReqGood))}
-                if(Requester) {aFilter.push(new Filter("Requester", FilterOperator.Contains, Requester))}
-                if(ReqDate) {aFilter.push(new Filter("ReqDate", FilterOperator.Contains, ReqDate))}
-                if(ReqStatus) {aFilter.push(new Filter("ReqStatus", FilterOperator.Contains, ReqStatus))}
+                if(ReqNum) {aFilter.push(new Filter("ReqNum", FilterOperator.Contains, ReqNum))} //ReqNum값이 존재할 경우 해당하는 값이 존재할 때 결과값으로 반환
+                if(ReqGood) {aFilter.push(new Filter("ReqGood", FilterOperator.Contains, ReqGood))} //ReqGood값이 존재할 경우 해당하는 값이 존재할 때 결과값으로 반환
+                if(Requester) {aFilter.push(new Filter("Requester", FilterOperator.Contains, Requester))} //Requester값이 존재할 경우 해당하는 값이 존재할 때 결과값으로 반환
+                if(ReqDate) {aFilter.push(new Filter("ReqDate", FilterOperator.Contains, ReqDate))} //ReqDate값이 존재할 경우 해당하는 값이 존재할 때 결과값으로 반환
+                if(ReqStatus) {aFilter.push(new Filter("ReqStatus", FilterOperator.Contains, ReqStatus))} //ReqStatus값이 존재할 경우 해당하는 값이 존재할 때 결과값으로 반환
                 
-                let oTable = this.byId("ui_table").getBinding("rows");
-                oTable.filter(aFilter);
+                let oTable = this.byId("ui_table").getBinding("rows"); //ui_table이라는 id값을 가진 테이블에 존재하는 데이터를 oData에 할당
+                oTable.filter(aFilter); // 테이블에 존재하는 데이터를 설정된 필터 조건으로 필터링
 
             },
+
+            //input 초기화 위한 함수
             onReset : function () {
-                this.byId("ReqNum").setValue("");
-                this.byId("ReqGood").setValue("");
-                this.byId("Requester").setValue("");
+                this.byId("ReqNum").setValue(""); 
+                this.byId("ReqGood").setValue(""); 
+                this.byId("Requester").setValue(""); 
                 this.byId("ReqStatus").setSelectedKey(""); 
                 this.byId("ReqDate").setValue(""); 
-                
-                this.onSearch();
+                //input 값 초기화
+                this.onSearch(); // this가 onSearch를 가리키도록 함
             },
-            // onAfterRendering: function(evt) {
-            //     var oTable = this.byId("ui_table");
-            //     var iIndex = oTable.getSelectedIndex();
-            //     var sMsg;
 
-            //     sMsg = oTable.getContextByIndex(iIndex);
-            //     addEventListener("click", function (evt) {
-            //         alert(sMsg);
-            //     });
-            // },
+            //정렬을 위한 함수
+            onSort : function () {
+                // load가 되어있다면 load를 시켜주고 실행, 아니라면 그냥 open
+                // 한번 실행 이후 fragment 호출 처리를 위한 조건문
+                if(!this.byId("SortDialog")){
+                    Fragment.load({
+                        id: this.getView().getId(), //어느 뷰에 fragment 띄울 것인지 결정
+                        // (즉 현재 화면에 띄우기 위해 현재 페이지 view id 불러옴)
+                        // id : application-project1-display-component---Request (npm으로 build 시)
+                        // 특정값으로 지정하지 않을 시 계속 상속하기 때문에 id값을 지정해줌
+                        name : "project1.view.fragment.SortDialog",
+                        // 불러올 fragment가 존재하는 경로
+                        controller : this
+                        // this를 현재 함수가 아닌 전체 controller를 가리키게 함
+                    }).then(function(oDialog){
+                        this.getView().addDependent(oDialog);
+                        // fragment는 생명주기와 model 존재 X. 뷰가 destory 될 때 자동으로 destory됨
+                        // 자식요소 즉 fragment를 부모 요소 즉 view의 생명주기 이벤트와 모델을 상속시킴
+                        // 로드했던 상태를 갖고 있으려고 view에 상속시킴
+                        oDialog.open("filter");
+                        // filter 필요 X, 삭제해도 정상적으로 작동
+                    }.bind(this));//this가 함수 안의 this라 적용 X
+                    //해결방안
+                    //1) bind(this) : 컨트롤러를 가리키게 함
+                    //2) 애초에 조건문 바깥에 this를 가리키는 변수 선언 -> 함수 안에서도 컨트롤러 전체를 의미하는 this로 사용
+                } else {
+                    this.byId("SortDialog").open("filter");
+                    
+                }
+                this.onSearch();
+                // 값이 유지될 경우 초기화 따로 X 사용
 
+            },
+
+            // 정렬 조건 선택 후 확인 버튼을 눌렀을 때 실행되는 함수
+            onConfirmSortDialog : function(oEvent){ 
+                let mParams = oEvent.getParameters(); //이벤트로 발생하는 파라미터, 즉 정렬 조건 받아 mParmas 변수에 저장
+                let sPath = mParams.sortItem.getKey(); // 받은 파라미터의 키값 받기
+                let bDescending = mParams.sortDescending; //오름차순, 내림차순 여부 설정을 받는 변수(true일 경우 내림차순 적용)
+
+                let aSorters = []; // 정렬 조건들을 한번에 저장하기 위한 배열  선언(정렬 순서와 정렬 기준, 2개가 존재하기 때문)
+
+                aSorters.push(new Sorter(sPath, bDescending)); // 배열에 정렬 조건(custom, 내림||오름차순)을 Sorter를 생성하여 push
+
+                let oBinding = this.byId("ui_table").getBinding("rows"); //ui_table이라는 id값을 가진 테이블에 존재하는 데이터를 oBinding에 할당
+                oBinding.sort(aSorters); // 테이블에 존재하는 데이터를 설정된 정렬 조건으로 정렬
+            },
+            onCreateOrder :function (){
+                this.getOwnerComponent().getRouter().navTo("CreateOrder");
+                //getOwnerComponent : manifest||Component에 있는 정보 불러올 때 사용하는 메소드
+                //getRouter : Router의 정보를 가져오고자 할 때 사용하는 메소드
+                //navTo : navigation To의 약자, routes의 name을 통해 이동
+
+                //주소줄에 입력해서 이동하고 싶을 시 #/CreateOrder -> # :해시 기반으로 타겟 찾아오기 때문
+
+            },
+            
+
+            //선택한 ropw의 indices 값 불러오는 함수
             getSelectedIndices : function () {
                 const rows = document.querySelector("button");
                 var aIndices = this.byId("ui_table").getSelectedIndices();
@@ -74,6 +125,7 @@ sap.ui.define([
     
             },
 
+            //선택한 ropw의 Context Index 값 불러오는 함수
             getContextByIndex: function() {
                 var oTable = this.byId("ui_table");
                 var iIndex = oTable.getSelectedIndex();
@@ -85,6 +137,7 @@ sap.ui.define([
                 }
                 MessageToast.show(sMsg);
             },
+
             //원하는 row값 출력
             getContext : function(){
                 var oTable = this.byId("ui_table");
